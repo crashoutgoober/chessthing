0128bcf04d04f3abb52ac0214fd533b5efef471e
yes <102892737+crashoutgoober@users.noreply.github.com>
2024-09-13 10:26:15 -0500

    Update index.js

--- index.js
+++ index.js
@@ -5,13 +5,21 @@ const letters = [
   "g", "h"
 ]
 
+    var engine = new Worker("stockfish.js");
+    console.log("GUI: uci");
+    engine.postMessage("uci");
+    console.log("GUI: ucinewgame");
+    engine.postMessage("ucinewgame");
+
+const printOut = (str) => document.write(str + "\n")
 const attemptMove = (moveName) => {
   var data = {};
-  
   data.main = 35.56
   data.other = 72.32
   
   compiledData[moveName] = data;
+
+  printOut(`${moveName}: ${data.main}%`)
 }
 const knightMoves = ["Na3", "Nh3", "Nf3", "Nc3"]
 
@@ -21,4 +29,3 @@ letters.forEach((value, index) => {
     var letter = value.toUpperCase()
     attemptMove(letter + "3"); attemptMove(letter + "4");
 })
-console.log(compiledData)
